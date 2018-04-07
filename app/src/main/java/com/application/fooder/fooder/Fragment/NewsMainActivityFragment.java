edacdc8b7c5f34bd369ad7f0fb0cb41e06971276
Mark Vincha <mvincha404@gmail.com>
2018-04-07 11:37:23 +0300

    Login and signup pages are ready.

--- app/src/main/java/com/application/fooder/fooder/Fragment/NewsMainActivityFragment.java
+++ app/src/main/java/com/application/fooder/fooder/Fragment/NewsMainActivityFragment.java
@@ -16,7 +16,7 @@ public class NewsMainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_news_main_activity, container, false);
     }
 
